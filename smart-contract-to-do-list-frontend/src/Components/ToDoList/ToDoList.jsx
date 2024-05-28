533ede87be37301f44759d78476d9084abed0514
elliott <48373238+Satchel05@users.noreply.github.com>
2024-05-28 14:07:15 -0700

    delete comments, start on delete function

--- smart-contract-to-do-list-frontend/src/Components/ToDoList/ToDoList.jsx
+++ smart-contract-to-do-list-frontend/src/Components/ToDoList/ToDoList.jsx
@@ -7,17 +7,17 @@ const ToDoList = () => {
 
   const [toDoItems, setToDoItems] = useState([]);
 
-  const changeToDoStatus = (toDoItem) => {
-    console.log("Testing");
-    // just a deep copy, don't let it psyche you out
-    let updatedToDoItems = [...toDoItems, toDoItem];
-    // Why do we need this?
-    // updatedToDoItems[index][1] = !updatedToDoItems[index][1];
+  const changeToDoStatus = (index) => {
+    let updatedToDoItems = [...toDoItems];
+    updatedToDoItems[index][1] = !updatedToDoItems[index][1];
     setToDoItems(updatedToDoItems);
   };
 
-  const deleteToDoItem = (index) => {
-    console.log("Checked");
+  const deleteToDoItem = (element) => {
+    // Filter method: https://react.dev/learn/updating-arrays-in-state
+    setToDoItems(toDoItems.filter((value, index) => {
+      
+    }))
   };
 
   return (
